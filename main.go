430200546d2587ec8e8e6e54af894efdd1ffddd8
Schatz, Jurgen J <jurgen.schatz@standardbank.co.za>
2019-05-20 17:23:11 +0200

    Input validation

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"github.com/pkg/errors"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -15,75 +15,55 @@ type Zone struct {
 }
 
 type Rover struct {
-	CurrentX int
-	CurrentY int
+	CurrentX         int
+	CurrentY         int
 	CurrentDirection string
-	Zone Zone
+	Zone             Zone
 }
 
 func main() {
 
-	zone := ReadZoneDimentions()
+	zone := ReadZoneDimensions()
 
-	rover :=  Rover{3,5,"E", zone}
+	xStart, yStart := zone.ReadRoverStartingCoOrdinates()
+
+	rover := Rover{xStart, yStart, "E", zone}
 
 	move := ReadRoverMoveOrders()
 
 	rover.MoveRover(move)
 
-	rover.Zone.CreateZone()
-
 	log.Println("------------------------------------")
 	log.Println("| Rover ending co-ordinates        |")
 	log.Println("------------------------------------")
-	log.Printf("X Postition: %v\nY Position: %v\nCurrent Direction: %v", rover.CurrentX, rover.CurrentY, rover.CurrentDirection)
+	log.Printf("X Axis: %v", rover.CurrentX)
+	log.Printf("Y Axis: %v", rover.CurrentY)
+	log.Printf("Direction facing: %v", rover.CurrentDirection)
 }
 
-func (z *Zone)CreateZone(){
-	var ar  []string
-
-	log.Println("---------------------------")
-	log.Println("| Your zone:              |")
-	log.Println("---------------------------")
-	for q := z.X; q > 0 ; q-- {
-		ar = append(ar, strconv.Itoa(q))
-		for p := z.Y; p > 0; p-- {
-			ar = append(ar, " ")
-		}
-		log.Println(ar)
-		ar = []string{}
-	}
-	yAxisAray := []string{"0"}
-
-	for x := 1; z.Y >= x ; x++ {
-		yAxisAray = append(yAxisAray, strconv.Itoa(x))
-	}
-	log.Println(yAxisAray)
-}
-
-func (r *Rover)MoveRover(movement string) {
+func (r *Rover) MoveRover(movement string) {
 
 	log.Println("------------------------------------")
 	log.Println("| Rover movement log               |")
 	log.Println("------------------------------------")
 
-	for _ , m := range movement {
+	for _, m := range movement {
 
 		if strings.EqualFold(r.CurrentDirection, "N") {
 			if strings.EqualFold(string(m), "M") {
 				if r.CurrentX < r.Zone.X {
-				log.Println("Moving North 1 space")
-				r.CurrentX++
-				continue
-				}else {
+					log.Println("Moving North 1 space")
+					r.CurrentX++
+					continue
+				} else {
 					log.Println("Rover is leaving current zone. Signal lost!")
 					panic(errors.New("Rover has exited current zone. Signal lost!"))
 				}
-			}else if strings.EqualFold(string(m), "R") {
+			} else if strings.EqualFold(string(m), "R") {
 				log.Println("Changing direction to East")
 				r.CurrentDirection = "E"
 				continue
-			}else if strings.EqualFold(string(m), "L") {
+			} else if strings.EqualFold(string(m), "L") {
 				log.Println("Changing direction to West")
 				r.CurrentDirection = "W"
 				continue
@@ -95,15 +75,15 @@ func (r *Rover)MoveRover(movement string) {
 					log.Println("Moving East 1 space")
 					r.CurrentY++
 					continue
-				}else {
+				} else {
 					log.Println("Rover is leaving current zone. Signal lost!")
 					panic(errors.New("Rover has exited current zone. Signal lost!"))
 				}
-			}else if strings.EqualFold(string(m), "R") {
+			} else if strings.EqualFold(string(m), "R") {
 				log.Println("Changing direction to South")
 				r.CurrentDirection = "S"
 				continue
-			}else if strings.EqualFold(string(m), "L") {
+			} else if strings.EqualFold(string(m), "L") {
 				log.Println("Changing direction to North")
 				r.CurrentDirection = "N"
 				continue
@@ -115,15 +95,15 @@ func (r *Rover)MoveRover(movement string) {
 					log.Println("Moving South 1 space")
 					r.CurrentX--
 					continue
-				}else {
+				} else {
 					log.Println("Rover is leaving current zone. Signal lost!")
 					panic(errors.New("Rover is leaving current zone. Signal lost!"))
 				}
-			}else if strings.EqualFold(string(m), "R") {
+			} else if strings.EqualFold(string(m), "R") {
 				log.Println("Changing direction to West")
 				r.CurrentDirection = "W"
 				continue
-			}else if strings.EqualFold(string(m), "L") {
+			} else if strings.EqualFold(string(m), "L") {
 				log.Println("Changing direction to East")
 				r.CurrentDirection = "E"
 				continue
@@ -135,15 +115,15 @@ func (r *Rover)MoveRover(movement string) {
 					log.Println("Moving West 1 space")
 					r.CurrentY--
 					continue
-				}else {
+				} else {
 					log.Println("Rover is leaving current zone. Signal lost!")
 					panic(errors.New("Rover is leaving current zone. Signal lost!"))
 				}
-			}else if strings.EqualFold(string(m), "R") {
+			} else if strings.EqualFold(string(m), "R") {
 				log.Println("Changing direction to North")
 				r.CurrentDirection = "N"
 				continue
-			}else if strings.EqualFold(string(m), "L") {
+			} else if strings.EqualFold(string(m), "L") {
 				log.Println("Changing direction to South")
 				r.CurrentDirection = "S"
 				continue
@@ -154,31 +134,87 @@ func (r *Rover)MoveRover(movement string) {
 	log.Println("------------------------------------")
 }
 
-func ReadZoneDimentions() Zone{
+func ReadZoneDimensions() Zone {
 
-	reader := bufio.NewReader(os.Stdin)
+	var x int
+	var y int
 
 	log.Println("Please enter Zone X axis length:")
-	xAxis, _ := reader.ReadString('\n')
-	x, _ := strconv.Atoi(xAxis)
+	_, err := fmt.Scanf("%d", &x)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	log.Println("Please enter Zone Y axis length:")
-	yAxis, _ := reader.ReadString('\n')
-	y, _ := strconv.Atoi(yAxis)
+	_, err = fmt.Scanf("%d", &y)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	zone := Zone{x, y}
 
 	return zone
 }
 
-func ReadRoverMoveOrders()string{
+func (z *Zone) ReadRoverStartingCoOrdinates() (int, int) {
+	var x int
+	var y int
 
-	reader := bufio.NewReader(os.Stdin)
+	for {
+		log.Println("Please enter Rover starting X axis:")
+		_, err := fmt.Scanf("%d", &x)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if x > z.X {
+			log.Printf("This point is outside of the current zone X axis of: %v", z.X)
+			continue
+		}
+		break
+	}
 
-		log.Println("Please enter Rover movement orders:")
-
-		move, _ := reader.ReadString('\n')
-
-		return move
+	for {
+		log.Println("Please enter Rover starting Y axis:")
+		_, err := fmt.Scanf("%d", &y)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if y > z.Y {
+			log.Printf("This point is outside of the current zone Y axis of: %v", z.Y)
+			continue
+		}
+		break
+	}
+	return x, y
 }
 
+func ReadRoverMoveOrders() string {
+
+	var move string
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		log.Println("Please enter Rover movement orders:")
+
+		move, _ = reader.ReadString('\n')
+		move = strings.TrimSuffix(move, "\n")
+
+		if !ConfirmRoverMoveOrdersValidity(move) {
+			log.Println("Rover movement order input is not correct. Only 'M', 'R' or 'L' permitted")
+			continue
+		}
+		break
+	}
+	return move
+}
+
+func ConfirmRoverMoveOrdersValidity(orders string) bool {
+	const alpha = "mlr"
+
+	for _, char := range orders {
+		if !strings.Contains(alpha, strings.ToLower(string(char))) {
+			return false
+		}
+	}
+	return true
+}
